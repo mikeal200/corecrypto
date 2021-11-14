ae6f6fcd48d4258666e9d66fdacb9cb5bdab8ccf
mikeal200 <willi6mc@mail.uc.edu>
2021-11-14 12:53:58 -0500

    fixed ccaes_vng_ccm.h file not found

---
+++ build.linux/ccaes/src/vng/ccaes_vng_ccm_encrypt.d
@@ -0,0 +1,8 @@
+dependencies: ccaes/src/vng/ccaes_vng_ccm_encrypt.c \
+  cc/corecrypto/cc_runtime_config.h cc/corecrypto/cc_config.h \
+  cc/corecrypto/ccaes_vng_ccm.h ccaes/corecrypto/ccaes.h \
+  ccmode/corecrypto/ccmode.h cc/corecrypto/cc.h \
+  ccmode/corecrypto/ccmode_impl.h ccmode/corecrypto/ccmode_siv.h \
+  cccmac/corecrypto/cccmac.h ccmode/corecrypto/ccmode_internal.h \
+  ccmode/corecrypto/ccmode_factory.h ccn/corecrypto/ccn.h \
+  cc/corecrypto/cc_priv.h cc/corecrypto/cc_macros.h
